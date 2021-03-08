f03034c7e91766b652b2edc8559f2293cc7b8da4
andypike7 <tech@sym.ru>
2021-03-08 09:44:34 +0300

    Clean up

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <h1>Blockchain Websocket</h1>
+    <Panel />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Panel from "@/components/Panel";
 
 export default Vue.extend({
   name: "App",
   components: {
-    HelloWorld
+    Panel
   }
 });
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Arial;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
